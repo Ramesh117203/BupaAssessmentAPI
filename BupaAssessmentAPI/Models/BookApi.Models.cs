ed981cd65942d01fc7abb69dd93b78d39d8202ab
Ramesh117203 <ramesh.konda532@gmail.com>
2024-10-18 14:11:12 +0530

    Changes committed

---
+++ BupaAssessmentAPI/Models/BookApi.Models.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BookApi.Models
+{
+    public class BookOwner
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public List<Book> books { get; set; }
+    }
+
+
+    public class Book
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+}
